db295ccdb4b1bfbf3ec6bbaece4a6dab2a372a08
Quantum-Monarch <kentwoiso@gmail.com>
2025-07-02 17:23:09 +0200

    fixed settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 
-
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,11 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY","fallback-secret-key") #'django-insecure-q^1o$$ixr)at_iahzk)3$r4qh-%j++ms2ng5gvq2ic1ynozfbg'
+SECRET_KEY = os.getenv("SECRET_KEY","fallback-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =os.getenv("DEBUG", "True") == True
-#DEBUG=True
+
 ALLOWED_HOSTS = ['*']
 
 
@@ -78,11 +78,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+
 }
 
 
